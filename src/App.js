767872e56bbaad2474e3bbe8cdada7a0c89074b0
chaudilip <dilipchau3602@gmail.com>
2023-02-03 19:39:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React , {useState} from 'react';
+
+//first value second is function
+
 
 function App() {
+  ///To get local Time 
+  let now = new Date().toLocaleTimeString();
+  //First we set the time and second function in useState we set the value
+  const [time,setTime] = useState(now);
+  const [count,setCount] = useState(12);
+  function updateTime(){
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+  setInterval(updateTime,1000);
+  function increase(){
+    setCount(count + 1);
+  }
+  function decrease(){
+    setCount(count - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='container'>
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+      <br />
+   <h1>{time}</h1>
+   <br />
+   <button onClick={updateTime}>Get Time</button>
+   </div>
+
   );
 }
 
